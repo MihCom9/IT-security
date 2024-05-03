7448b8406193e4c851298f80f2e434e425cf5ed6
Stefan Stefanov <stefi_bg@outlook.com>
2024-05-03 19:06:18 +0300

    Add Home page, css and animation

---
+++ home.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('button');
+    btn.addEventListener('mouseenter', move);
+    console.log('btn handler added');
+});
+
+
+
+function move() {
+    const btn = document.getElementById('button');
+
+    const top = Math.random() * (document.defaultView.innerHeight - btn.clientHeight);
+    const left = Math.random() * (document.defaultView.innerWidth - btn.clientWidth);
+    btn.style.position = 'absolute';
+    btn.style.top = `${top}px`;
+    btn.style.left = `${left}px`;
+}
